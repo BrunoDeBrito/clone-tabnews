b889db3bce462656b7294a62c03d63d6a57651ff
BrunoDeBrito <brunordebrito@gmail.com>
2025-07-11 11:38:29 -0300

    ajuste na mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O verdadeiro valor do amor, e mais impactante mensagem validada.</h1>
+    return<h1>Cada dia mais, quero estar ao seu lado não importa a situação, o momento, todos os dias eu vou te amar sempre e muito! Te amo Meu amor ❤️❤️</h1>
 }
 
 export default Home
