c3ad89756c1c6cf039eaf5f416a32be0f3b6be9f
BrunoDeBrito <brunordebrito@gmail.com>
2025-07-18 13:27:31 -0300

    add  and  and ours

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Renata, eu amo você. Se você me ama, dá uma risadinha! 😎</h1>;
+  return (
+    <h1>
+      Cada dia mais, quero estar ao seu lado não importa a situação, o momento,
+      todos os dias eu vou te amar sempre e muito! Te amo Meu amor ❤️❤️
+    </h1>
+  );
 }
 
 export default Home;
