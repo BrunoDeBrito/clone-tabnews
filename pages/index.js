de9316b88c52797cb9072355cae8f18e5b12fcdf
BrunoDeBrito <brunordebrito@gmail.com>
2025-07-18 16:58:20 -0300

    testing

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Cada dia mais, quero estar ao seu lado não importa a situação, o momento,
-      todos os dias eu vou te amar sempre e muito! Te amo Meu amor ❤️❤️
+      todos os dias eu vou te amar sempre e muito! Te amo Meu amor ❤️❤️ Marcella Tainã
     </h1>
   );
 }
