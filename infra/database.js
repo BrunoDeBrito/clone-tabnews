77a24b32e730cb5dd9f33582fce8ba4bfd9a9890
BrunoDeBrito <brunordebrito@mail.com>
2025-09-01 23:55:32 -0300

    feat: create and use `InternalSErver` Error in `/api/v1/status`

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,9 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log("\n Erro dentro do catch do database.js");
     console.error("Database query error:", error);
+    throw error;
   } finally {
     await client?.end();
   }
