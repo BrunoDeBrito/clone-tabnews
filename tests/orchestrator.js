72126b092a6d19d79fee857bb5ebcde710bb6263
BrunoDeBrito <brunordebrito@gmail.com>
2025-12-04 18:15:15 -0300

    feat: ajustes

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -56,7 +56,7 @@ async function createUser(userObject) {
     username:
       userObject?.username || faker.internet.username().replace(/[_.-]/g, ""),
     email: userObject?.email || faker.internet.email(),
-    password: userObject?.password || "validpassword",
+    password: userObject?.password || "validPassword",
   });
 }
 
